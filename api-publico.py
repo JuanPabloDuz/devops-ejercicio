35f1af95777b0c1c123ffd3bd2d553c1a6048bf2
JuanPabloDuz <juanpabloduz@gmail.com>
2023-11-13 11:44:36 -0300

    slim shady

--- api-publico.py
+++ api-publico.py
@@ -3,6 +3,6 @@ import json
 def lambda_handler(event, context):
     response = {
         "statusCode": 200,
-        "body": json.dumps("Hola desde AWS Lambda! Mi nombre es... Juan Pablo"),
+        "body": json.dumps("Hola desde AWS Lambda! Mi nombre es... Slim Shady"),
     }
     return response
